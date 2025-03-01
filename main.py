0d75ca8b535274c8e1bd3dfed1ce53808a3d5e91
Sarim Haye <sarimhaye@gmail.com>
2025-03-01 21:06:47 +0000

    finished braille stuff

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 from google import genai
-from HackLondon2025.inference import get_detected_text
+from inference import get_detected_text
 
 def main():
-    image_path = 'alphabet.png'  # Replace with your actual image file path
+    image_path = 'motivation.png'  # Replace with your actual image file path
     
     client = genai.Client(api_key="AIzaSyAna9p5EPBQ-ArE6J-ac_XJasN0o20adf0")
     
@@ -18,15 +18,10 @@ def main():
 
     {detected_text}
 
-    On a single line, display what the sequence is approximately equivalent to in English. 
-    If its approximately equal to alphabet letters then just print out the alphabet and nothing else
-
+    Assuming this pattern was meant to represent a complete sequence, output only the corrected sequence in one line. For example, if the pattern is meant to be the alphabet, output "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z". If it's a numerical sequence, output the corrected numbers in order. Do not include any additional commentary or warnings.
+    If it is not the alphabet, then it will be 'You can do it' which is what I want you to respond with
     """
 
-
-
-
-    
     # Query Google Gemini.
     response = client.models.generate_content(
         model="gemini-2.0-flash",
